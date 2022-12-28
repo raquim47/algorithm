e74dcd3a9c87b999eccaf4115523b291ae258d47
jin <raquim47@naver.com>
2022-12-28 22:27:07 +0900

    Update rsp

--- rsp/rsp.js
+++ rsp/rsp.js
@@ -1,10 +1,20 @@
 const $result = document.querySelector('.result');
 const $computer = document.querySelector('#computer');
 const $user = document.querySelector('#user');
-const $scissors = document.querySelector('#scissors');
-const $rock = document.querySelector('#rock');
-const $paper = document.querySelector('#paper');
+
+const $handBtns = document.querySelectorAll('.hands button');
+const $scissorsBtn = document.querySelector('#scissors');
+const $rockBtn = document.querySelector('#rock');
+const $paperBtn = document.querySelector('#paper');
+const $resetBtn = document.querySelector('#reset');
+
 const score = { scissors: 1, rock: 2, paper: 3 };
+// score[scissors] - score[paper] === -2; lose
+// score[scissors] - score[rock] === -1; win
+// score[rock] - score[scissors] === 1; lose
+// score[rock] - score[paper] === -1; win
+// score[paper] - score[rock] === 1; lose
+// score[paper] - score[scissors] === 2; win
 
 let computerHand = 'scissors';
 
@@ -19,23 +29,36 @@ const changeHand = () => {
   $computer.src = `img/${computerHand}.png`;
   $user.src = `img/${computerHand}.png`;
 };
-const intervalId = setInterval(changeHand, 50);
+let intervalId = setInterval(changeHand, 50);
 
 const judgeGame = (myHand) => {
-  if (computerHand === myHand) {
-    console.log('hi');
-    return '비겼습니다';
+  const game = score[computerHand] - score[myHand];
+  if (game === -2 || game === 1) {
+    return '졌습니다';
+  } else if (game === -1 || game === 2) {
+    return '이겼습니다';
   } else {
-    console.log('hi');
-    return 'ap';
+    return '비겼습니다';
   }
 };
 
-const clickBtn = (e) => {
+const clickHandBtn = (e) => {
   clearInterval(intervalId);
   const myHand = e.target.dataset.hand;
   $user.src = `img/${myHand}.png`;
   $result.innerText = judgeGame(myHand);
+  $handBtns.forEach(button => button.disabled = true);
+  $resetBtn.disabled = false;
 };
 
-$scissors.addEventListener('click', clickBtn);
+const clickReset = (e) => {
+  $handBtns.forEach(button => button.disabled = false);
+  intervalId = setInterval(changeHand, 50);
+  e.target.disabled = true;
+}
+
+$handBtns.forEach(button => button.addEventListener('click', clickHandBtn));
+// $scissorsBtn.addEventListener('click', clickBtn);
+// $rockBtn.addEventListener('click', clickBtn);
+// $paperBtn.addEventListener('click', clickBtn);
+$resetBtn.addEventListener('click', clickReset)
